009bd4c416e6b15edfb0c6cb238fc7d3c1b36a4a
sseohyunbum-ops <s.seohyunbum@gmail.com>
2026-07-11 13:38:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yeongi-v3';
+const CACHE = 'yeongi-v4';
 const SHELL = [
   './',
   './index.html',
@@ -8,7 +8,7 @@ const SHELL = [
   './icon-512.png',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-];
+  ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
@@ -17,8 +17,8 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+    .then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', (e) => {
@@ -33,5 +33,5 @@ self.addEventListener('fetch', (e) => {
       }
       return res;
     }))
-  );
+    );
 });
